c8513e7577f0654cb41ab67fcd57d3f0f60cd3bf
Dyadyushko Marat <Dwarf-2008@bk.ru>
2023-05-20 00:46:48 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// // Задача 2: Напишите программу, 
+// которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите число");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num>num2) {
+// Console.WriteLine(num);
+// } else {
+// Console.WriteLine(num2);
+// }
+
+// Задача 4: Напишите программу, 
+// которая принимает на вход три числа
+//  и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите 3 числа:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2 > max)
+// {
+// max = num2;
+// }
+
+// if (num3 > max)
+// {
+// max = num3;
+// }
+
+// Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
+// //Задача 6: Напишите программу, 
+// которая на вход принимает число и выдаёт
+// , является ли число чётным 
+// (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if ((num % 2) == 0)
+// {
+// Console.WriteLine("Четное");
+// }
+// else
+// {
+// Console.WriteLine("Нечетное");
+// }
+
+// Задача 8: Напишите программу, 
+// которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int evenNumber = 2;
+
+// if(num > 1)
+// {
+// while(evenNumber <= num)
+// {
+// Console.Write(evenNumber + " ");
+// evenNumber = evenNumber + 2;
+// }
+// }
+
