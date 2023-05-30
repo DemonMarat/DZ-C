fd70022a223c294fb8769aa6e8a2a398ba6caf6e
Dyadyushko Marat <Dwarf-2008@bk.ru>
2023-05-30 22:08:16 +0300

    dz charp1

--- Program.cs
+++ Program.cs
@@ -188,4 +188,52 @@
     
 // }
 
+// Задача 25: Напишите цикл, 
+// который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
 
+// Console.WriteLine("введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int step = a;
+
+// for (int i = 1; i < b; i++)
+// {
+// step = step * a;
+// }
+// Console.WriteLine("A в степени B равно: " + step);
+
+
+// Задача 27: Напишите программу,
+//  которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.WriteLine("введите число");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (i > 0)
+// {
+// int num = i % 10;
+// i = i / 10;
+// sum = sum + num;
+// }
+// Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
+// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int [] numbers = new int[8];
+// Console.Write("[");
+
+// for (int i = 0; i < numbers.Length; i++)
+//  {
+//     numbers [i] = new Random().Next(0, 11);
+//     Console.Write(" " + Method (i) + " ");
+//  }
+// Console.Write("]");
+
+// int Method (int a)
+// {
+//     return numbers[a];
+// }
