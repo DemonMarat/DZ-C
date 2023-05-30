075c4d206aabe7354caff424e90ccf1b167f6e8a
Dyadyushko Marat <Dwarf-2008@bk.ru>
2023-05-30 19:55:43 +0300

    dz charp

--- Program.cs
+++ Program.cs
@@ -129,4 +129,63 @@
 
 // CheckingTheDayOfTheWeek(dayNumber);
 
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+
+// int numLength = numText.Length;
+
+// int count = 0;
+
+
+// while (numLength == 5)
+// {
+//     if (numText[0] == numText[4] && numText[1] == numText[3])
+//     {
+//         Console.WriteLine($"{numText} - Палиндром");
+//     } else {
+//         Console.WriteLine($"{numText} - Не палиндром");
+//     }
+
+//     return;
+// }
+// Console.WriteLine($"{numText} - Введите 5 чисел!");
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Console.WriteLine("Введите число");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// double sum = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+// Console.WriteLine(sum);
+// 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// Console.WriteLine("Введите числo");
+// int num= Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while (num >= count)
+// {
+// Console.Write(Math.Pow(count,3) + " ; ");
+//     count++;
+    
+// }
+
 
