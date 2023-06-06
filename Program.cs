5afd2cc56144d24f4c4c9514430ce548f28739fe
Dyadyushko Marat <Dwarf-2008@bk.ru>
2023-06-06 22:07:04 +0300

    dz charp6

--- Program.cs
+++ Program.cs
@@ -359,3 +359,44 @@
 //     Console.Write("]");
 //     Console.WriteLine();
 // }
+
+// Задача 41: Пользователь вводит
+// с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+ 
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями 
+//  y = k1 * x + b1, y = k2 * x + b2;
+//   значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
